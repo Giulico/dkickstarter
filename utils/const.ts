e27cafc7ea9770facfcf98ad8f0acde1335091fd
Giulio Collesei <giuliocollesei@gmail.com>
2023-01-23 15:43:01 +0100

    use always the remote host

--- utils/const.ts
+++ utils/const.ts
@@ -1,4 +1,5 @@
-export const BASE_HOST: string =
-  process.env.NEXT_PHASE === 'phase-production-build'
-    ? 'https://dkickstarter.vercel.app'
-    : 'http://localhost:3000'
+// export const BASE_HOST: string =
+//   process.env.NEXT_PHASE === 'phase-production-build'
+//     ? 'https://dkickstarter.vercel.app'
+//     : 'http://localhost:3000'
+export const BASE_HOST = 'https://dkickstarter.vercel.app'
